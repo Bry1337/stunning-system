cab8a99afa6beda8af2b89641085037b27fa005b
Edward Bryan Abergas <edwardbryan.abergas@gmail.com>
2018-11-12 10:54:48 +0800

    Updated base application and title repository.

--- app/src/main/java/io/github/bry1337/noted/application/BaseApplication.kt
+++ app/src/main/java/io/github/bry1337/noted/application/BaseApplication.kt
@@ -2,8 +2,12 @@ package io.github.bry1337.noted.application
 
 import android.app.Application
 import androidx.work.Constraints
+import androidx.work.ExistingPeriodicWorkPolicy
 import androidx.work.NetworkType.UNMETERED
 import androidx.work.PeriodicWorkRequest
+import androidx.work.WorkManager
+import io.github.bry1337.noted.activities.main.RefreshMainDataWork
+import java.util.concurrent.TimeUnit
 
 /**
  * Created by Edward Bryan Abergas on 08/11/2018.
@@ -16,19 +20,25 @@ import androidx.work.PeriodicWorkRequest
  */
 class BaseApplication : Application() {
 
-	override fun onCreate() {
-		super.onCreate()
-	}
+    override fun onCreate() {
+        super.onCreate()
+    }
 
-	private fun setupWorkManagerJob() {
-		// Use the constraints to require the work to run only when the device is charging
-		// and the network is unmetered
-		val constraints = Constraints.Builder()
-				.setRequiresCharging(true)
-				.setRequiredNetworkType(UNMETERED)
-				.build()
+    private fun setupWorkManagerJob() {
+        // Use the constraints to require the work to run only when the device is charging
+        // and the network is unmetered
+        val constraints = Constraints.Builder()
+            .setRequiresCharging(true)
+            .setRequiredNetworkType(UNMETERED)
+            .build()
 
-		// Specify that the work should run everyday
+        // Specify that the work should run everyday
+        val work = PeriodicWorkRequest.Builder(RefreshMainDataWork::class.java, 1, TimeUnit.DAYS)
+            .setConstraints(constraints)
+            .build()
 
-	}
+        // Enqueue it with WorkManager, keeping any previously scheduled jobs for the same work.
+        WorkManager.getInstance()
+            .enqueueUniquePeriodicWork(RefreshMainDataWork::class.java.name, ExistingPeriodicWorkPolicy.KEEP, work)
+    }
 }
--- app/src/main/java/io/github/bry1337/noted/repositories/TitleRepository.kt
+++ app/src/main/java/io/github/bry1337/noted/repositories/TitleRepository.kt
@@ -1,5 +1,7 @@
 package io.github.bry1337.noted.repositories
 
+import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.Transformations
 import io.github.bry1337.noted.databases.TitleDao
 import io.github.bry1337.noted.networks.MainNetwork
 
@@ -20,6 +22,83 @@ import io.github.bry1337.noted.networks.MainNetwork
  * a network API and an offline database cache.
  */
 class TitleRepository(private val network: MainNetwork,
-	private val titleDao: TitleDao) {
+                      private val titleDao: TitleDao) {
+
+    /**
+     * [LiveData] to load title.
+     *
+     * This is the main interface for loading the title. The title will be loaded
+     * from the offlien cache.
+     *
+     * Observing this will not cause the title to be refreshed. use [TitleRepository.refreshTitle]
+     * to refresh the title.
+     *
+     * Because this is defined as `by lazy` it won't be instantiated until the property id used
+     * for the first time.
+     */
+    val title: LiveData<String> by lazy<LiveData<String>>(LazyThreadSafetyMode.NONE) {
+        Transformations.map(titleDao.loadTitle()) {
+            it.title
+        }
+    }
+
+    fun refreshTitle(onStateChanged: TitleStateListener) {
+
+    }
+
+    /**
+     * Class that represents the state of a refresh request.
+     *
+     * Sealed class can only be extended from inside this file.
+     */
+    // TODO: Remove this class after rewriting refreshTitle
+    sealed class RefreshState {
+
+        /**
+         * The request is currently loading.
+         *
+         * An object is a singleton that cannot have more than one instance.
+         */
+        object Loading : RefreshState()
+
+        /**
+         * The request has completed successfully.
+         *
+         * An object is a singleton that cannot have more than one instance.
+         */
+        object Success : RefreshState()
+
+        /**
+         * The request has completed with an error.
+         *
+         * @param throwable error message ready to be displayed.
+         */
+        class Error(val throwable: Throwable) : RefreshState()
+    }
 
 }
+
+/**
+ * Listener for [RefreshState] changes.
+ *
+ * A typealias introduces a shorthand way to say a complex type.
+ * It does not create a new type.
+ */
+// TODO: Remove this typealias after rewriting refreshTitle
+typealias TitleStateListener = (TitleRepository.RefreshState) -> Unit
+
+/**
+ * Thrown when there was an error fetching a new title.
+ *
+ * @property message user ready error message
+ * @property cause the original cause of this exception
+ */
+class TitleRefreshError(cause: Throwable) : Throwable(cause.message, cause)
+
+/**
+ * Suspend function to use callback-based [FakeNetworkCall] in coroutines.
+ *
+ * @return network result after completion.
+ * @throws throwable original exception from the library if network request fails.
+ */
+// TODO: Implement FakeNetworkCall<T>.wait() here
